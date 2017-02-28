158a5bb721f63e89b3fd8e33f76aefedaf4cc63a
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-02-28 14:00:28 -0800

    Add some linting
    So as to be moar awesome!

--- zorgborg.js
+++ zorgborg.js
@@ -1,47 +1,36 @@
 require('dotenv').config();
 if (!process.env.SLACK_TOKEN) {
-    console.log('Error: Specify SLACK_TOKEN in environment');
-    process.exit(1);
+  throw new Error('Error: Specify SLACK_TOKEN in environment');
 }
-
 if (!process.env.CLEVER_BOT_USER) {
-    console.log('Error: Specify CLEVER_BOT_USER in environment');
-    process.exit(1);
+  throw new Error('Error: Specify CLEVER_BOT_USER in environment');
 }
 if (!process.env.CLEVER_BOT_API_KEY) {
-    console.log('Error: Specify CLEVER_BOT_API_KEY in environment');
-    process.exit(1);
+  throw new Error('Error: Specify CLEVER_BOT_API_KEY in environment');
 }
 
-var cleverbot = require("cleverbot.io"),
-cleverbot = new cleverbot(process.env.CLEVER_BOT_USER, process.env.CLEVER_BOT_API_KEY);
+const Cleverbot = require("cleverbot.io");
+let cleverbot = new Cleverbot(process.env.CLEVER_BOT_USER, process.env.CLEVER_BOT_API_KEY);
 cleverbot.setNick("zorgbort");
-cleverbot.create(function (err, session) {
-    if (err) {
-        console.log('cleverbot create fail.');
-    } else {
-        console.log('cleverbot create success.');
-    }
+cleverbot.create(err => {
+  console.error(`cleverbot error: ${err}`);
 });
 
-var Botkit = require('botkit');
-var os = require('os');
-
-var controller = Botkit.slackbot({
-    debug: true,
+const Botkit = require('botkit');
+let controller = Botkit.slackbot({
+  debug: true,
 });
-
-var bot = controller.spawn({
-    token: process.env.SLACK_TOKEN
+controller.spawn({
+  token: process.env.SLACK_TOKEN
 }).startRTM();
 
 controller.hears('','direct_message,direct_mention,mention',function(bot,message) {
-    var msg = message.text;
-    cleverbot.ask(msg, function (err, response) {
-        if (!err) {
-            bot.reply(message, response);
-        } else {
-            console.log('cleverbot err: ' + err);
-        }
-    });
+  let msg = message.text;
+  cleverbot.ask(msg, function (err, response) {
+    if (!err) {
+      bot.reply(message, response);
+    } else {
+      console.error(`cleverbot error: ${err}`);
+    }
+  });
 });
