0d41ad8042688a72cef7d71ec1a7eeda20360900
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-02-28 21:46:41 -0800

    Add more conversation features

--- src/conversation.js
+++ src/conversation.js
@@ -1,4 +1,79 @@
+/*
+  Many thanks to https://github.com/howdyai/botkit/blob/master/slack_bot.js
+  Where most of this was stolen from
+*/
+
 const excuse = require('huh');
+const os = require('os');
+
+const hi = (bot, message) => {
+  bot.api.reactions.add({
+    timestamp: message.ts,
+    channel: message.channel,
+    name: 'robot_face',
+  }, function(err) {
+    if (err) {
+      bot.botkit.log('Failed to add emoji reaction :(', err);
+    }
+  });
+  bot.storage.users.get(message.user, function(err, user) {
+    if (user && user.name) {
+      bot.reply(message, 'Hello ' + user.name + '!!');
+    } else {
+      bot.reply(message, 'Hello.');
+    }
+  });
+};
+
+const shutdown = (bot, message) => {
+  bot.startConversation(message, function(err, convo) {
+    convo.ask('Are you sure you want me to shutdown?', [
+      {
+        pattern: bot.utterances.yes,
+        callback: function(response, convo) {
+          convo.say("Alright Sir, if you'll not be needing me, I'll close down for awhile");
+          convo.next();
+          setTimeout(function() {
+            process.exit();
+          }, 3000);
+        }
+      },
+      {
+        pattern: bot.utterances.no,
+        default: true,
+        callback: function(response, convo) {
+          convo.say('*ZORGBORT LIVES!*');
+          convo.next();
+        }
+      }
+    ]);
+  });
+};
+
+const uptime = (bot, message) => {
+  let formatUptime = function(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+      uptime = uptime / 60;
+      unit = 'minute';
+    }
+    if (uptime > 60) {
+      uptime = uptime / 60;
+      unit = 'hour';
+    }
+    if (uptime != 1) {
+      unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+  };
+  const hostname = os.hostname();
+  const uptime = formatUptime(process.uptime());
+  const reponse = ':robot_face: I am a bot named <@' + bot.identity.name +
+          '>. I have been running for ' + uptime + ' on ' + hostname + '.';
+  bot.reply(message, reponse);
+};
 
 const defaultExcuse = (bot, message) => {
   let msg = message.text;
@@ -7,5 +82,8 @@ const defaultExcuse = (bot, message) => {
 };
 
 module.exports = bot => {
+  bot.hears(['hello', 'hi', 'howdy', 'sup', 'howzit'], hi);
+  bot.hears(['shutdown', 'powerdown'], shutdown);
+  bot.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], uptime);
   bot.hears('','direct_message,direct_mention,mention', defaultExcuse);
 };
