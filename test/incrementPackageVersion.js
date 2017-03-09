1d2441eb3c9e2563d1775c5496d197c75f304908
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-03-09 00:18:55 -0800

    Added function to update the package.json version
    This will allow us to do our own work and no farm it out to bash

---
+++ test/incrementPackageVersion.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const incrementPackageVersion = require('../lib/incrementPackageVersion');
+const assert = require('assert');
+const fs = require('mz/fs');
+const appRoot = require('app-root-path');
+
+describe('Increment Package Version', function() {
+  const tmpDir = `${appRoot}/tmp`;
+  const filePath = `${tmpDir}/package.json`;
+
+  beforeEach('create package.json file', async function() {
+    await fs.writeFile(filePath, JSON.stringify({
+      version: '1.0.0'
+    }));
+  });
+  afterEach('remove package.json file', async function() {
+    await fs.unlink(filePath);
+  });
+  it('Increments Major', async function() {
+    const result = await incrementPackageVersion(tmpDir, 'major');
+    const targetVersion = '2.0.0';
+    assert.equal(result, targetVersion);
+    const json = await fs.readFile(filePath);
+    const data = JSON.parse(json);
+    assert.equal(data.version, targetVersion);
+  });
+  it('Increments Minor', async function() {
+    const result = await incrementPackageVersion(tmpDir, 'minor');
+    const targetVersion = '1.1.0';
+    assert.equal(result, targetVersion);
+    const json = await fs.readFile(filePath);
+    const data = JSON.parse(json);
+    assert.equal(data.version, targetVersion);
+  });
+  it('Increments Patch', async function() {
+    const result = await incrementPackageVersion(tmpDir, 'patch');
+    const targetVersion = '1.0.1';
+    assert.equal(result, targetVersion);
+    const json = await fs.readFile(filePath);
+    const data = JSON.parse(json);
+    assert.equal(data.version, targetVersion);
+  });
+});
