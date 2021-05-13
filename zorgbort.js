eab868ebf15180bd2682c3114b626b2c5fbec6dc
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-05-13 12:29:45 -0700

    Remove botkit wrapper code and old linting config

--- zorgbort.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-require('dotenv').config();
-
-const bot = require('./lib/bot.js');
-
-require('./src/releases.js')(bot);
-require('./src/releaseAndTag.js')(bot);
-require('./src/dogs.js')(bot);
-require('./src/conversation.js')(bot);
