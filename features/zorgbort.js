808a8159d8686beaf164f4facffa3012b644d6ca
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-06-30 15:57:32 -0700

    rm obsolete cheese name functionality.

--- features/zorgbort.js
+++ features/zorgbort.js
@@ -1,5 +1,4 @@
 'use strict';
-const randomCheese = require('../src/cheese');
 const randomDogbreed = require('../src/dogs');
 const { defaultExcuse, hi, listUsers, uptime }  = require('../src/conversation');
 const { listReleases, releaseInteraction } = require('../src/releases');
@@ -13,7 +12,6 @@ module.exports = controller => {
   controller.hears(['hello', 'hi', 'howdy', 'sup', 'howzit'], mention, hi);
   controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'], mention, uptime);
   controller.hears(['list users'], mention, listUsers);
-  controller.hears(['cheese?'], mention, randomCheese);
   controller.hears(['dog?'], mention, randomDogbreed);
   controller.hears('list releases', mention, listReleases);
   controller.hears(['start release', 'release'], mention, startRelease);
--- src/cheese.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const cheeseName = require('cheese-name');
-
-const randomCheese = async (bot, message) => {
-  await bot.reply(message, cheeseName());
-};
-
-module.exports = randomCheese;
